36469498ef88866fc8e320916cd1bfb3c58de769
Scorpio493.8 <Scorpio493.8@gmail.com>
2024-05-31 00:06:37 +0800

    init repo

---
+++ src/main/java/com/scorpio4938/animalhandbook/AnimalHandbookClient.java
@@ -0,0 +1,10 @@
+package com.scorpio4938.animalhandbook;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AnimalHandbookClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/scorpio4938/animalhandbook/AnimalHandbookDataGenerator.java
@@ -0,0 +1,11 @@
+package com.scorpio4938.animalhandbook;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AnimalHandbookDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
