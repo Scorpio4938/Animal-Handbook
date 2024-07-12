a9475125f93661b79865414b8c25d9c3f1cdeacc
Scorpio4938 <mac15@email.com>
2024-07-12 11:20:22 +0800

    modified the test text of the custom book screen and added a page

--- src/main/java/com/scorpio4938/animalhandbook/gui/screen/CustomBookScreen.java
+++ src/main/java/com/scorpio4938/animalhandbook/gui/screen/CustomBookScreen.java
@@ -1,21 +1,19 @@
 package com.scorpio4938.animalhandbook.gui.screen;
 
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.BookScreen;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.nbt.NbtString;
-import net.minecraft.text.Text;
 
 public class CustomBookScreen extends BookScreen {
-    private static final String DEFAULT_TEXT_1 = "Default Text";
+    private static final String DEFAULT_TEXT_1 = "Default Text page 1 :)";
+    private static final String DEFAULT_TEXT_2 = "Default Text page 2 :D";
     private ItemStack book;
 
     public CustomBookScreen(PlayerInventory playerInventory, ItemStack book) {
         super(new WrittenBookContents(book));
-//        super(new CustomBook);
         this.book = book;
         setDefaultText(book);
     }
@@ -34,20 +32,27 @@ public class CustomBookScreen extends BookScreen {
 
     private void setDefaultText(ItemStack book) {
         /*
+         * Solved
          * Bug: This return '* invalid book tag *'
          */
+
+        // [ai's way] Create the NBT compound to store the book data.
+//        NbtCompound bookTag = new NbtCompound();
         NbtCompound nbt = book.getOrCreateNbt();
+
+        // Set the title, and author of the book.
         nbt.putString("author", "Author");
         nbt.putString("title", "Custom Book");
 //        nbt.putString("pages", "[{\"text\":\"" + DEFAULT_TEXT_1 + "\"}]");
-//        nbt.putString("content", DEFAULT_TEXT_1);
 
-        // Create a list for the pages
+        // Create a list for the pages.
         NbtList pages = new NbtList();
-        pages.add(NbtString.of(Text.Serializer.toJson(Text.of("This is page 2"))));
+        pages.add(NbtString.of(DEFAULT_TEXT_1));
+        pages.add(NbtString.of(DEFAULT_TEXT_2));
 
-        // Add the pages to the book tag
+        // Add the pages to the book tag.
         nbt.put("pages", pages);
+        
         book.setNbt(nbt);
     }
 
